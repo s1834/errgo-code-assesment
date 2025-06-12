a9eac6fa870e5ef5451fa14b6905976cdb5d1b7f
s1834 <s18324@protonmail.com>
2025-06-12 22:42:32 +0530

    frontend+backend non bonus tasks completed

---
+++ Backend/src/app.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import { IProject } from "./models/project.interface";
+import { v4 as uuid } from "uuid";
+
+const app = express();
+const PORT = 3000;
+// List of projects
+const projects: IProject[] = [];
+
+// Setup cors and express.json()
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Errgo Backend Interview Module Loaded Successfully!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.post("/projects", (req, res) => {
+  const uuidString = uuid();
+
+  let hash = 0;
+  for (let i = 0; i < uuidString.length; i++) {
+    hash = hash * 31 + uuidString.charCodeAt(i);
+    hash = hash >>> 0; // Ensure positive integer
+  }
+  const newProject: IProject = {
+    id: hash,
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  projects.push(newProject);
+  res.status(200).json(newProject);
+});
+
+app.get("/projects", (req, res) => {
+  res.status(200).json(projects);
+});
